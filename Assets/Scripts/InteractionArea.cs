1a105b72ea416a0e42d581980ffae7e4b2b474e6
echununer <echununerpro@gmail.com>
2026-05-21 11:54:55 -0300

    clase 2

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class InteractionArea : MonoBehaviour
 {
+    public int score = 0;
+    public UIManager uiManagerScript;
+
+    void Awake()
+    {
+        uiManagerScript = GameObject.FindObjectOfType<UIManager>();
+    }
     void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.CompareTag("Collectible")){
             Destroy (col.gameObject);
+            score ++;
+            uiManagerScript.UpdateScore(score);
         }
-        Debug.Log (col.gameObject.name);
+    
     }
 }
