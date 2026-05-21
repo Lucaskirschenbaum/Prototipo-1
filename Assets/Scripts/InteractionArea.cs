12aab21377e1b5fe56b2c8cbdd1737f392902254
echununer <echununerpro@gmail.com>
2026-05-21 10:36:48 -0300

    Initial commit

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("Collectible")){
+            Destroy (col.gameObject);
+        }
+        Debug.Log (col.gameObject.name);
+    }
+}
